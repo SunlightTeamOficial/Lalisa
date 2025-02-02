aa20229ef57511212c20de245ecae71d51c04da8
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-02 15:28:20 -0500

    Add files via upload

---
+++ plugins/main-canal.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://i.ibb.co/3N4StyG/file.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `⭒─ׄ─ׅ─ׄ Canal Oficial
+─ׄ─ׅ─ׄ⭒
+
+*🪷* ${global.canal}
+
+> [ ✰ ] ${global.textbot}`
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
+}
+handler.help = ['canal']
+handler.tags = ['main']
+handler.command = /^(canal)$/i
+export default handler 
