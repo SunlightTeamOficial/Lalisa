d21642cf2002aa00ca50145e3d1be8b3f43a5486
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-02 20:56:37 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,31 +1,17 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys'
+//_welcome.js
+
+import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
-export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true
-
-  let who = m.messageStubParameters[0]
-  let taguser = `@${who.split('@')[0]}`
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/29vqco8m')
+  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
 
-  if (chat.welcome) {
-    let img;
-    try {
-      let pp = await conn.profilePictureUrl(who, 'image');
-      img = await (await fetch(pp)).buffer();
-    } catch {
-      img = await (await fetch(defaultImage)).buffer();
-    }
-
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🍬 *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `🍬 *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
-    }
-  }
-
-  return true
-}
+  if (chat.bienvenida && m.messageStubType == 27) {
+    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenid@s espero tu estadía sea linda acá 💗  
+ │✑  ${groupMetadata.subject}\n └───────────────┈ ⳹`
+    
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+}}
