ad7def4d71a3b966f1626c947b2339bdf4351538
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-02 21:03:03 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,17 +1,39 @@
-//_welcome.js
-
 import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/29vqco8m')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/sJk7RCBc/file.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenid@s espero tu estadía sea linda acá 💗  
- │✑  ${groupMetadata.subject}\n └───────────────┈ ⳹`
+    let bienvenida = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
+
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, canal)
+  }
+
+  if (chat.bienvenida && m.messageStubType == 28) {
+    let bye = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
     
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+await conn.sendAi(m.chat, botname, textbot, bye, img, canal)
+  }
+
+  if (chat.bienvenida && m.messageStubType == 32) {
+    let kick = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
+    
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+await conn.sendAi(m.chat, botname, textbot, kick, img, canal)
 }}
