1d0ec15c259d8b101c18f278e77ef95b90a5a4d3
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 15:29:34 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,23 +3,37 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/jYQH.jpg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/sJk7RCBc/file.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `27:  *꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
+    let bienvenida = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
 
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal)
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `28: *꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
+    let bye = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
+    
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `32: *꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Kicked* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
-  
-await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
+    let kick = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
+    
+╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
+┊•*⁀➷ °⭒⭒⭒
+│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
+╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal)
 }}
