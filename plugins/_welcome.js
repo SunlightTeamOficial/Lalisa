f4eb5c6c82d5fb554533f753727dd29d6b86073f
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-02 22:03:42 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,35 +4,33 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
   if (!m.messageStubType || !m.isGroup) return
 
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/sJk7RCBc/file.jpg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/p0JpJ6G/file.jpg')
   let img = await (await fetch(pp)).buffer()
 
-   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
+  let mensajes = {
+    27: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
 
-╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
-┊•*⁀➷ °⭒⭒⭒
-│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
-╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+    28: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
 
-  if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
-    
-╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
-┊•*⁀➷ °⭒⭒⭒
-│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
-╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
-
-  if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*
-    
-╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱
-┊•*⁀➷ °⭒⭒⭒
-│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*
-╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+    32: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Kicked* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
   }
 
   if ([27, 28, 32].includes(m.messageStubType)) {
-    await conn.sendMessage(m.chat, { image: img, caption: mensajes[m.messageStubType] }, { quoted: null })
+    await conn.sendMessage(m.chat, { 
+      image: img, 
+      caption: mensajes[m.messageStubType], 
+      contextInfo: {
+        externalAdReply: {
+          mediaUrl: null,
+          mediaType: 1,
+          showAdAttribution: true,
+          title: "📢 Ver canal",
+          body: "Únete al canal oficial",
+          previewType: 0,
+          thumbnail: img, // Usa la misma imagen de perfil
+          sourceUrl: "https://chat.whatsapp.com/tu-enlace-del-canal"
+        }
+      }
+    }, { quoted: null })
   }
 }
