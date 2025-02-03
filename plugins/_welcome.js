319b2fa3a5af1b06d198c340d365bfa6e9f1f145
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-02 22:13:14 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -8,11 +8,11 @@ export async function before(m, { conn }) {
   let img = await (await fetch(pp)).buffer()
 
   let mensajes = {
-    27: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
+    27:  *꒰︵፝֟⏜ᩨ︵ᡴꪫ Welcome User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Welcome* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
 
-    28: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
+    28: *꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Bye* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`,
 
-    32: `*꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Kicked* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
+    32: *꒰︵፝֟⏜ᩨ︵ᡴꪫ Goodbye User ꒰︵ᩨ⏜︵፝֟𑁯ᰍ*\n\n╭──ׅ─| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱\n┊•*⁀➷ °⭒⭒⭒\n│ ‹‹ *Kicked* :: *@${m.messageStubParameters[0].split`@`[0]}⁨*\n╰───| ͜͡  ͜͡ᩙ‎ | ͜͡  ͜͡ᩙ‎ | ͜͡ ꒱`
   }
 
   if ([27, 28, 32].includes(m.messageStubType)) {
@@ -28,7 +28,7 @@ export async function before(m, { conn }) {
           body: "Únete al canal oficial",
           previewType: 0,
           thumbnail: img, // Usa la misma imagen de perfil
-          sourceUrl: "https://whatsapp.com/channel/0029Vb2NkWWFsn0ghn9mOA2G"
+          sourceUrl: "https://chat.whatsapp.com/tu-enlace-del-canal"
         }
       }
     }, { quoted: null })
