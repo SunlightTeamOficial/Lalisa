137c00bd1303a4d951b5fc47e5e74f07eccf3be6
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 22:05:47 -0500

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -2,14 +2,15 @@ let handler = async (m, { conn, isOwner }) => {
 let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
-「 ❀ Usuarios Baneados 」
-│  ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
-│  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+╭─「 ❀ Usuarios Baneados 」
+│ >  ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
+│ >  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+╰────
 
-「 ❀ Chats Baneados 」
+╭─「 ❀ Chats Baneados 」
 │  ❀ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
 │  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
-
+╰────
 `.trim()
 m.reply(caption, null, {mentions: conn.parseMention(caption)})}
 handler.help = ['banlist']
