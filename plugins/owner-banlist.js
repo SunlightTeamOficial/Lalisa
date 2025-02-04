96e96396ccadc9fd6c85d322065b17e9a1f571fe
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 22:06:43 -0500

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -3,8 +3,8 @@ let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
 ╭─「 ❀ Usuarios Baneados 」
-│ >  ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
-│ >  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+> │  ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
+> │  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 ╰────
 
 ╭─「 ❀ Chats Baneados 」
