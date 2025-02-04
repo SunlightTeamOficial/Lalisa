39934ce20a68212cae050c55473ab69cb79ddc15
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-04 17:22:47 -0500

    Update downloader-apkmod.js

--- plugins/downloader-apkmod.js
+++ plugins/downloader-apkmod.js
@@ -5,7 +5,7 @@ let result = await res.json();
 let { name, size, lastUpdate, icon } = result;
 let URL = result.dllink
 let packe = result.package
-let texto = ` > ❀ » Nombre : ${name}
+let texto = `> ❀ » Nombre : ${name}
 > ❀ » Tamaño : ${size}
 > ❀ » Package : ${packe}
 > ❀ » Actualizado : ${lastUpdate}`
