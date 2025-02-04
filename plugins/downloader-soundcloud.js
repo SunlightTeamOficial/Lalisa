d02ef4cb1ae8eaafb51f11b73c2a3ac898627139
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 19:46:10 -0500

    Update downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `🚩 Ingrese el nombre de la cancion de *Soundcloud.*`, m, rcanal)
+if (!text) return conn.reply(m.chat, `❀ Ingrese el nombre de la cancion de *Soundcloud.* junto al comando`, m, rcanal)
 
 await m.react('🕒');
 try {
@@ -21,7 +21,6 @@ let txt = `*\`- S O U N C L O U D - M U S I C -\`*\n\n`;
     txt += `	✩  *Título* : ${json[0].title}\n`;
     txt += `	✩  *Calidad* : ${quality}\n`;
     txt += `	✩  *Url* : ${url}\n\n`;
-    txt += `> 🚩 *${textbot}*`
 
 await conn.sendFile(m.chat, image, 'thumbnail.jpg', txt, m, null, rcanal);
 await conn.sendMessage(m.chat, { audio: audio, fileName: `${json[0].title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
