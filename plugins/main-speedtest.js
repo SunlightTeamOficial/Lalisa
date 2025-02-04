a88f8d6540d8628f76a7e883e3e9c6316760fe71
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 21:52:34 -0500

    Update main-speedtest.js

--- plugins/main-speedtest.js
+++ plugins/main-speedtest.js
@@ -11,13 +11,12 @@ let handler = async (m, { conn }) => {
     let downloadSpeed = test.download?.bandwidth ? `${(test.download.bandwidth / 125000).toFixed(2)} Mbit/s` : 'No disponible';
     let uploadSpeed = test.upload?.bandwidth ? `${(test.upload.bandwidth / 125000).toFixed(2)} Mbit/s` : 'No disponible'
 
-    let txt = '`乂  S P E E D - T E S T`\n\n'
-        txt += `	✩   *Hosted By* : ${serverName}\n`
-        txt += `	✩   *Ubicación* : ${serverLocation}\n`
-        txt += `	✩   *Ping* : ${ping}\n`
-        txt += `	✩   *Speed Descarga* : ${downloadSpeed}\n`
-        txt += `	✩   *Speed Subida* : ${uploadSpeed}\n\n`
-        txt += `> 🚩 ${textbot}`
+    let txt = '`❀  S P E E D - T E S T`\n\n'
+        txt += `	❀   *Hosted By* : ${serverName}\n`
+        txt += `	❀   *Ubicación* : ${serverLocation}\n`
+        txt += `	❀   *Ping* : ${ping}\n`
+        txt += `	❀   *Speed Descarga* : ${downloadSpeed}\n`
+        txt += `	❀   *Speed Subida* : ${uploadSpeed}\n\n`
 
     await conn.reply(m.chat, txt, m, rcanal)
     await m.react('✅')
