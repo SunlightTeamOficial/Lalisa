086dbf6de4394c2c60fa7492bffd7f225061471e
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-04 16:17:02 -0500

    Update search-githubsearch.js

--- plugins/search-githubsearch.js
+++ plugins/search-githubsearch.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { text, command, usedPrefix }) => {
-  if (!text) return conn.reply(m.chat, '🚩 Ingresa el término de búsqueda en GitHub.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* LaLisa`, m, rcanal);
+  if (!text) return conn.reply(m.chat, '❀ Ingresa lo que deseas buscar junto al comando.`, m, rcanal);
   
   await m.react('🕓');
 
@@ -14,20 +14,20 @@ let handler = async (m, { text, command, usedPrefix }) => {
     if (res.status !== 200) throw json;
 
     if (json.items.length > 0) {
-      let txt = '`乂  G I T H U B  -  B Ú S Q U E D A`\n\n';
+      let txt = '`❀  GITHUB - BÚSQUEDA`\n\n';
       
       json.items.forEach((repo, i) => {
-        txt += `    ✩  *Nro* : ${i + 1}\n`;
-        txt += `    ✩  *Nombre del Repositorio* : ${repo.full_name}\n`;
-        txt += `    ✩  *URL* : ${repo.html_url}\n`;
-        txt += `    ✩  *Creado en* : ${formatDate(repo.created_at)}\n`;
-        txt += `    ✩  *Última actualización* : ${formatDate(repo.updated_at)}\n`;
-        txt += `    ✩  *Watchers* : ${repo.watchers}\n`;
-        txt += `    ✩  *Forks* : ${repo.forks}\n`;
-        txt += `    ✩  *Estrellas* : ${repo.stargazers_count}\n`;
-        txt += `    ✩  *Issues Abiertos* : ${repo.open_issues}\n`;
-        txt += `    ✩  *Descripción* : ${repo.description || 'Sin descripción'}\n`;
-        txt += `    ✩  *Clone* : \`\`\`$ git clone ${repo.clone_url}\`\`\`\n\n`;
+        txt += `> ❀ » Nro : ${i + 1}\n`;
+        txt += `> ❀ » Nombre del Repositorio : ${repo.full_name}\n`;
+        txt += `> ❀ » URL : ${repo.html_url}\n`;
+        txt += `> ❀ » Creado en : ${formatDate(repo.created_at)}\n`;
+        txt += `> ❀ » Última actualización : ${formatDate(repo.updated_at)}\n`;
+        txt += `> ❀ » Watchers : ${repo.watchers}\n`;
+        txt += `> ❀ » Forks : ${repo.forks}\n`;
+        txt += `> ❀ » Estrellas : ${repo.stargazers_count}\n`;
+        txt += `> ❀ » Issues Abiertos : ${repo.open_issues}\n`;
+        txt += `> ❀ » Descripción : ${repo.description || 'Sin descripción'}\n`;
+        txt += `> ❀ » Clone : \`\`\`$ git clone ${repo.clone_url}\`\`\`\n\n`;
       });
       
       await m.reply(txt);
