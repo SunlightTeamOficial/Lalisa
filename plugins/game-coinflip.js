433805152dce4fa4c68e5705235bdcc8a1a68c96
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 20:22:37 -0500

    Update game-coinflip.js

--- plugins/game-coinflip.js
+++ plugins/game-coinflip.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
     
     if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
         let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-        m.reply(`[ ✰ ] Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente.`)
+        m.reply(`❀ Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente.`)
         return
     }
 
     if (!text || !['cara', 'cruz'].includes(text.toLowerCase())) {
-        return conn.reply(m.chat, '[ ✰ ] Elige una opción ( *Cara o Cruz* ) para lanzar la moneda.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* cara`, m, rcanal)
+        return conn.reply(m.chat, '❀ Elige una opción ( *Cara o Cruz* ) para lanzar la moneda.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* cara`, m, rcanal)
     }
 
     cooldowns[m.sender] = Date.now()
