c3003757eda8a7bdaacc4a98feebf8189020a269
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-03 22:09:17 -0500

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -3,12 +3,12 @@ let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
 「 Usuarios Baneados 」
->  ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
->  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+>   ❀ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
+>   ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 
 「 Chats Baneados 」
->  ❀ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
->  ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+>   ❀ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
+>   ❀ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 `.trim()
 m.reply(caption, null, {mentions: conn.parseMention(caption)})}
 handler.help = ['banlist']
