9ee7c220dea27958e3d312b0ec5b61e64608a2d2
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-04 17:06:30 -0500

    Update main-canal.js

--- plugins/main-canal.js
+++ plugins/main-canal.js
@@ -4,9 +4,9 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://i.ibb.co/7tYbhL18/file.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `Sunlight Team Oficial Channel
+let txt = `𝚂𝚞𝚗𝚕𝚒𝚐𝚑𝚝 𝚃𝚎𝚊𝚖 𝙾𝚏𝚒𝚌𝚒𝚊𝚕
 
-*❀* ${global.canal}`
+${global.canal}`
 
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
