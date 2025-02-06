d3c14c437086d28ad878af9d15086a38d97f25ed
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-06 16:14:08 -0500

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `> *_❒ Alquiler finalizado_*`)
+        await this.reply(m.chat, `> *_❒ Alquiler Finalizado_*`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
