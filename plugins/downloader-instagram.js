f00cb6ee0ccc87275e787512b6e77101dd3b0ef3
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-07 13:04:09 -0500

    Update downloader-instagram.js

--- plugins/downloader-instagram.js
+++ plugins/downloader-instagram.js
@@ -17,7 +17,7 @@ let mediaUrl = media.url
 let HS = `> *_❀ » Duración: ${duration}_*`
       
 if (media.metadata.isVideo) {
-await conn.sendFile(m.chat, mediaUrl[0], 'instagram.mp4', HS, m, null, rcanal)
+await conn.sendFile(m.chat, mediaUrl[0], 'instagram.mp4', HS, m)
 } else {
 for (let url of mediaUrl) {
 await conn.sendFile(m.chat, url, 'instagram.jpg', HS, m)
