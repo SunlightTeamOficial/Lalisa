2e4dce2b20d453fe8a3feea1e719e5a8a1b64717
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-08 15:31:18 -0500

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-return conn.reply(m.chat,`> *_❒ El Amor De ${text} Por Ti Es De ${Math.floor(Math.random() * 100)}% de un 100%
+return conn.reply(m.chat,`> *_❒ El Amor De ${text} Por Ti Es De ${Math.floor(Math.random() * 100)}% de un 100%_*
 `, m, rcanal)
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love *@user*']
