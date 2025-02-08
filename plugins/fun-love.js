d9ba248a8c59b684ba700c864713b280c71fafc5
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-08 15:27:29 -0500

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-if (!args[0]) return conn.reply(m.chat,`*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+return conn.reply(m.chat,`*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
 `, m, rcanal)
