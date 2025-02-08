0cfbc4dd9ce9d3dfe0f82a06017584a1f72c5933
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-08 15:05:54 -0500

    Update fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -6,7 +6,6 @@ let b
 do b = ps.getRandom()
 while (b === a)
 return conn.reply(m.chat,`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, m, rcanal)
-mentions: [a, b]
 })}
 handler.help = ['formarpareja']
 handler.tags = ['fun']
