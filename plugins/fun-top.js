24ecdd3f276a6ad9a93a9133de29fcac3bc1b597
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-08 19:38:54 -0500

    Update fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -17,7 +17,7 @@ let j = ps.getRandom()
 let k = Math.floor(Math.random() * 70);
 let x = `${pickRandom(['🤓','😅','😂','😳','😎', '🥵', '😱', '🤑', '🙄', '💩','🍑','🤨','🥴','🔥','👇🏻','😔', '👀','🌚'])}`
 let l = Math.floor(Math.random() * x.length);
-let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
+if (!text) let return conn.reply(m.chat,`https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 let top = `*${x} Top 10 ${text} ${x}*
     
 *1. ${user(a)}*
@@ -29,7 +29,7 @@ let top = `*${x} Top 10 ${text} ${x}*
 *7. ${user(g)}*
 *8. ${user(h)}*
 *9. ${user(i)}*
-*10. ${user(j)}*`
+*10. ${user(j)}*`, m, rcanal)
 m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
 handler.help = handler.command = ['top']
