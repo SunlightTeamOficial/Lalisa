f22828e534c86bfb0318027de61906d0439be59f
SunlightTeamOficial <sunlightteamoficialchannel@gmail.com>
2025-02-08 19:07:09 -0500

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -1,8 +1,8 @@
-let handler = async (m, { command, text }) => m.reply(`
+let handler = async (m, { command, text }) => return conn.reply(m.chat,`
 *⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
   
 *𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
+*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}, m, rcanal)
 `.trim(), null, m.mentionedJid ? {
 mentions: m.mentionedJid
 } : {})
